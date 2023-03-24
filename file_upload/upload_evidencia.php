8eef43e1a06b796b034c5741e34821f836e6d402
FcoRipJr <57280475+FcoRipJr@users.noreply.github.com>
2023-03-24 09:35:03 -0400

    Update upload_evidencia.php

--- file_upload/upload_evidencia.php
+++ file_upload/upload_evidencia.php
@@ -1,7 +1,6 @@
 
 
 <?php
-// Diretório onde os arquivos serão armazenados
 $usuario = $_POST['usuario'] ?? null;
 $dir_usuario = $usuario == null ? '' : $usuario.'/' ;
 
@@ -13,22 +12,16 @@ if (!file_exists($dir)) {
     mkdir($dir, 0777, true);
 }
 
-// Tamanho máximo do arquivo (em bytes)
 $tamanho_maximo = 10485760; // 10 MB
 
-// Array com as extensões permitidas
 $extensoes_permitidas = array('pdf', 'jpg', 'png', 'doc', 'jpeg');
 
-// Verifica se um arquivo foi enviado
 if (!empty($_FILES["arquivo"])) {
-    // Verifica se o arquivo é uma imagem
     if (in_array(strtolower(pathinfo($_FILES["arquivo"]["name"], PATHINFO_EXTENSION)), $extensoes_permitidas)) {
-        // Verifica o tamanho do arquivo
+        
         if ($_FILES["arquivo"]["size"] <= $tamanho_maximo) {
-            // Nome do arquivo será a concatenação das variáveis enviadas no formulário, mais o timestamp do upload
             $nome_arquivo = $usuario . '_' . $resposta . '_' . date('Y_m_d') . '.' . pathinfo($_FILES["arquivo"]["name"], PATHINFO_EXTENSION);
 
-            // Move o arquivo enviado para o diretório
             if (move_uploaded_file($_FILES["arquivo"]["tmp_name"], $dir . $nome_arquivo)) {
                 echo "Arquivo enviado com sucesso.";
             } else {
@@ -44,5 +37,5 @@ if (!empty($_FILES["arquivo"])) {
     echo "Nenhum arquivo enviado.";
 }
 ?>
-
+<br>
 <a href="index.php">voltar</a>
